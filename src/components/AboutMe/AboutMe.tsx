26c1371daf6258360fe1e9b57ac8aeceaeafa3de
n3trnnr <n3trnnr@gmail.com>
2024-10-10 05:47:40 +0300

    адаптирование секции AboutMe

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './AboutMe.module.scss'
+import styles from './index.module.scss'
 import photo from '../../assets/photo.png'
 
 //Приветствую! Я фронтенд разработчик и я могу сделать для вас классные программистские штуки, а пока вы думаете можете тут осмотреться.
@@ -9,9 +9,8 @@ const AboutMe = () => {
         <section className={styles.section}>
             <div className={styles['section__inner']}>
                 <div className={styles['section__text-content']}>
-                    <h1 className={styles['section__headling']}>Alexey Serdyuk <br /> Frontend Developer</h1>
+                    <h1 className={styles['section__headling']}>Alexey Serdyuk <br /> <span>Frontend Developer</span></h1>
                     <p className={styles['section__description']}>Hello! I'm a frontend developer and I can do cool programming stuff for you, and while you're thinking, you can look around here.</p>
-                    {/* <p className={styles['section__description']}>Приветствую! Я фронтенд разработчик и я могу сделать для вас классные программистские штуки, а пока вы думаете можете тут осмотреться.</p> */}
                 </div>
                 <img className={styles['section__image']} src={photo} alt="" />
             </div>
