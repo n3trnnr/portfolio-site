6a8b110f06b367a2e91e4421ac67b6c15856d2ac
n3trnnr <n3trnnr@gmail.com>
2024-10-16 04:07:42 +0300

    Исправлен duration на header

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -4,15 +4,16 @@ import { Iwork } from "./Work.props";
 const Work = ({ title, description, img, url }: Iwork) => {
 
     return (
-        <div className={styles['work']} data-aos="flip-up">
-            <p className={styles['work__description']}>{description}</p>
-
-            <a href={url} target='_blank'>
-                <img className={styles['work__work-image']} src={img} />
-            </a>
-            <a className={styles["work__work-title"]} href={url} target="_blank">
-                {title}
-            </a>
+        <div className={styles['work']} >
+            <div className={styles['work__inner']}>
+                <p data-aos='fade-down' className={styles['work__description']}>{description}</p>
+                <a data-aos='flip-up' href={url} target='_blank'>
+                    <img className={styles['work__work-image']} src={img} />
+                </a>
+                <a className={styles["work__work-title"]} href={url} target="_blank">
+                    {title}
+                </a>
+            </div>
         </div>
     );
 }
